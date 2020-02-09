2f2dbba6fcf5330359a8c7ab4e80576fe32deb64
BigtoC <taitocdt@gmail.com>
2020-02-09 22:04:05 +0800

    Added common functions

---
+++ Crawler/util.py
@@ -0,0 +1,17 @@
+# coding=utf-8
+
+from _datetime import datetime
+import time
+import pytz
+import random
+
+
+def print_time_and_msg(msg: str):
+    now_time = time.time()
+    readable_time = datetime.fromtimestamp(now_time, pytz.timezone('Asia/Shanghai')).strftime(f'[%H:%M:%S:%m] - {msg}')
+    print(readable_time)
+
+
+def random_wait():
+    wait_time = random.uniform(0.5, 1.5)
+    time.sleep(wait_time)
