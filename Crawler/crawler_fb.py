177b470544e6dfc92271266c7db8d3f68fa976e5
BigtoC <taitocdt@gmail.com>
2020-02-10 23:46:52 +0800

    Refined code structure and enhance crawling speed

--- Crawler/crawler_fb.py
+++ Crawler/crawler_fb.py
@@ -3,8 +3,8 @@ from typing import List, Any, Union
 
 import requests
 from pyppeteer import launch
-import asyncio
 import nest_asyncio
+import asyncio
 import pysnooper
 from Crawler.util import *
 
@@ -25,7 +25,7 @@ async def get_web_instance(url: str):
     await page.setViewport({'width': 1920, 'height': 1080})
     await page.goto(url, {'timeout': 10000 * 30})
 
-    print_time_and_msg(f"The browser is set!")
+    print_time_and_msg(f"The browser is set!\n")
     return page, browser
 
 
@@ -38,34 +38,32 @@ async def login(page):
     await page.type("#pass", password)
     random_wait()
 
-    await page.click("#u_0_3")
+    await page.click("#u_0_b")
     random_wait()
 
     await page.waitForNavigation()
+    asyncio.get_event_loop().run_until_complete(scroll_down(page))
+    return page
 
+
+async def scroll_down(page):
     for i in range(10):
         await page.evaluate('_ => {window.scrollBy(0, window.innerHeight);}')
         random_wait()
 
-    return page
 
+async def get_data_from_web(page, url: str) -> str:
+    await page.goto(url)
 
-async def get_data_from_web(url: str) -> str:
-    page, browser = asyncio.get_event_loop().run_until_complete(
-        get_web_instance(url)
-    )
-
-    after_login = asyncio.get_event_loop().run_until_complete(login(page))
     print_time_and_msg(f"Crawling contents...")
+    asyncio.get_event_loop().run_until_complete(scroll_down(page))
 
     content_str = ""
-    all_post_contents = await after_login.xpath('//*[@id="pagelet_timeline_main_column"]')
+    all_post_contents = await page.xpath('//*[@id="pagelet_timeline_main_column"]')
 
     for item in all_post_contents:
         # 获取文本
         content_str += await (await item.getProperty('textContent')).jsonValue()
-
-    await browser.close()
 
     return content_str
 
@@ -73,6 +71,6 @@ async def get_data_from_web(url: str) -> str:
 if __name__ == '__main__':
     nest_asyncio.apply()
     bonjour_url = "https://www.facebook.com/pg/bonjourhk/posts/"
-    asyncio.get_event_loop().run_until_complete(
-        get_data_from_web(bonjour_url)
-    )
+    # asyncio.get_event_loop().run_until_complete(
+    #     get_data_from_web(bonjour_url)
+    # )
--- Crawler/main.py
+++ Crawler/main.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 
 import time
-from Crawler.crawler_fb import get_data_from_web
+from Crawler.crawler_fb import login, get_web_instance, get_data_from_web
 from Crawler.data_handler import handler_selector
 from Crawler.util import *
 
@@ -20,7 +20,7 @@ def start_crawling():
     for shop, url in sources.items():
         print_time_and_msg(f"Start crawling {shop}")
         current_contents: str = asyncio.get_event_loop().run_until_complete(
-            get_data_from_web(url)
+            get_data_from_web(page, url)
         )
         handler_selector(shop, url, current_contents)
         print()
@@ -28,5 +28,11 @@ def start_crawling():
 
 if __name__ == "__main__":
     nest_asyncio.apply()
+
+    page, browser = asyncio.get_event_loop().run_until_complete(
+        get_web_instance("https://www.facebook.com/")
+    )
+
+    page = asyncio.get_event_loop().run_until_complete(login(page))
 
     start_crawling()
