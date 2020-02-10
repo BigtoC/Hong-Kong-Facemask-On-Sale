9ab71d316923abd1c2607f74cbe060015b91bef3
BigtoC <taitocdt@gmail.com>
2020-02-10 23:01:24 +0800

    Fixed cannot print chinese

--- Crawler/util.py
+++ Crawler/util.py
@@ -4,11 +4,17 @@ from _datetime import datetime
 import time
 import pytz
 import random
+import locale
+
+locale.setlocale(locale.LC_CTYPE, 'chinese')
 
 
 def print_time_and_msg(msg: str):
     now_time = time.time()
-    readable_time = datetime.fromtimestamp(now_time, pytz.timezone('Asia/Shanghai')).strftime(f'[%H:%M:%S:%m] - {msg}')
+    readable_time = datetime.fromtimestamp(
+        now_time,
+        pytz.timezone('Asia/Shanghai')
+    ).strftime(f'[%H:%M:%S:%m] - {msg}')
     print(readable_time)
 
 
